ae3c108408c4283e9029014468aa9e6c5fef0d76
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2021-02-21 15:20:17 +0900

    Add files via upload

---
+++ Odango１/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  Odango１
+//
+//  Created by 吉田光 on 2021/02/20.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Odango１/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Odango１
+//
+//  Created by 吉田光 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
