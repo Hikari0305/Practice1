cb35b5301fdd4fecfc16ac38038523b79a80b353
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2021-02-23 02:16:15 +0900

    Add files via upload

---
+++ odango/odango/PhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewController.swift
+//  odango UIKit
+//
+//  Created by 吉田光 on 2021/02/22.
+//
+
+import UIKit
+
+
+class PhotoViewController: ButtonBarPagerTabStripViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
