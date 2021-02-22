aa5b8878f0be076a94f1de188b90e79260283398
Hikari0305 <78633083+Hikari0305@users.noreply.github.com>
2021-02-22 15:55:00 +0900

    Add files via upload
    2月22日UIKit

---
+++ odango/odango/odango/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odango
+//
+//  Created by 吉田光 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+   
+}
+
